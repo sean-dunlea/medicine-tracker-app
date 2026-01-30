8c05ab0a23f071d9c6f5f4c09891669303fbf1c9
Sean Dunlea <122353906@umail.ucc.ie>
2026-01-30 15:39:50 +0000

    fix: add local notification preview to avoid FCM foreground focus issues

--- static/firebase.js
+++ static/firebase.js
@@ -18,47 +18,49 @@ const messaging = getMessaging(app);
 
 const enable_notifications_button = document.getElementById("enable_notifications");
 
-enable_notifications_button.addEventListener("click", async () => {
-    console.log("Requesting permission...");
-    const permission = await Notification.requestPermission();
-    if (permission === 'granted') {
-        console.log('Notification permission granted.');
-        alert("Notification permission granted.");
-        navigator.serviceWorker.register("/static/firebase-messaging-sw.js").then((registration) => {
-            getToken(messaging, { 
-                vapidKey: 'BEsK6dC7_GO_iZ2wtVLQouKCC0RgS2cuMKNWHBOUlk_4vlq1OBDTXb1FqWNOBH4TYBE8sZZpSCwzkYhy6WKIU5I',
-                serviceWorkerRegistration: registration
-            }).then((currentToken) => {
-                if (currentToken) {
-                    console.log("FCM Token: ", currentToken);
-                    fetch("/save_token", {
-                        method : "POST",
-                        headers : { "Content-Type" : "application/json" },
-                        body : JSON.stringify({ token : currentToken }) 
-                    }).then(response => {
-                        if (response.ok) {
-                            console.log("Token sent to server successfully")
-                            // Optionally update UI
-                            enable_notifications_button.style.display = "none";
-                        } else {
-                            console.error("Failed to send token to server")
-                        }
-                    }).catch(err => 
-                        console.error("Error sending token to server"))
-                } else {
-                    // Show permission request UI
-                    console.log('No registration token available. Request permission to generate one.');
+if (enable_notifications_button) {
+    enable_notifications_button.addEventListener("click", async () => {
+        console.log("Requesting permission...");
+        const permission = await Notification.requestPermission();
+        if (permission === 'granted') {
+            console.log('Notification permission granted.');
+            alert("Notification permission granted.");
+            navigator.serviceWorker.register("/static/firebase-messaging-sw.js").then((registration) => {
+                getToken(messaging, { 
+                    vapidKey: 'BEsK6dC7_GO_iZ2wtVLQouKCC0RgS2cuMKNWHBOUlk_4vlq1OBDTXb1FqWNOBH4TYBE8sZZpSCwzkYhy6WKIU5I',
+                    serviceWorkerRegistration: registration
+                }).then((currentToken) => {
+                    if (currentToken) {
+                        console.log("FCM Token: ", currentToken);
+                        fetch("/save_token", {
+                            method : "POST",
+                            headers : { "Content-Type" : "application/json" },
+                            body : JSON.stringify({ token : currentToken }) 
+                        }).then(response => {
+                            if (response.ok) {
+                                console.log("Token sent to server successfully")
+                                // Optionally update UI
+                                enable_notifications_button.style.display = "none";
+                            } else {
+                                console.error("Failed to send token to server")
+                            }
+                        }).catch(err => 
+                            console.error("Error sending token to server"))
+                    } else {
+                        // Show permission request UI
+                        console.log('No registration token available. Request permission to generate one.');
+                        // ...
+                    }
+                    }).catch((err) => {
+                    console.log('An error occurred while retrieving token. ', err);
                     // ...
-                }
-                }).catch((err) => {
-                console.log('An error occurred while retrieving token. ', err);
-                // ...
-                });
-        })
-    } else {
-        console.log('Notification permission denied.');
-    }
-});
+                    });
+            })
+        } else {
+            console.log('Notification permission denied.');
+        }
+    });
+}
 
 onMessage(messaging, (payload) => {
 new Notification(payload.notification.title, {
@@ -66,3 +68,12 @@ new Notification(payload.notification.title, {
     // icon: '/static/icon.png' We can add this later when we have an icon for the app
     });
 });
+
+const test_notification_button = document.getElementById("test_notification");
+
+if (test_notification_button) {
+    test_notification_button.addEventListener("click", async () => {
+        await fetch("/test_notification", { method: "POST" });
+        console.log("Received test notification");
+    })
+}

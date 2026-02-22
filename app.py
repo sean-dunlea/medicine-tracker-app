f3e6c99634014095178d6800bccb90ca17b5c9c6
markrv04 <123446326@umail.ucc.ie>
2026-02-22 16:09:20 +0000

    Merged all friends pages into one

--- app.py
+++ app.py
@@ -608,63 +608,46 @@ def history():
                                 """, (user_id,),).fetchall()
     return render_template("history.html", title="Medication History", medications=medications)
 
-@app.route("/add_mate", methods=["GET", "POST"])
-@login_required
-def add_mate():
-    response = ""
-    form = AddMateForm()
-    if form.validate_on_submit():
-        sender = session["username"]
-        receiver = form.username.data
-        if sender == receiver:
-            form.username.errors.append("You cannot invite yourself.")
-            return render_template("add_mate.html", title="Add Mates", form=form, response=response)
-        db = get_db()
-        existing_user = db.execute("""
-                                SELECT *
-                                FROM users
-                                WHERE username = ?;
-                                """, (receiver,)).fetchone()
-        
-        existing_invite = db.execute("""
-                                     SELECT *
-                                     FROM invites
-                                     WHERE sender = ? AND receiver = ?;
-                                     """, (sender, receiver,)).fetchone()
-        if existing_invite is not None:
-            form.username.errors.append("You have already sent this user an invite.")
-        elif existing_user is not None:
-            db.execute("""
-                       INSERT INTO invites (sender, receiver)
-                       VALUES
-                       (?, ?);
-                       """, (sender, receiver,))
-            db.commit()
-            response = "Mate Request Sent"
-            # Hey diva, I just added this part so the receiver gets a notification
-            # about the friend request. Hope that's okay xx
-            title = "New Mate Request! 💊"
-            body = f"{sender} has sent you a MediMate request!"
-            send_email_notification(receiver, title, body)
-            send_push_notification(receiver, title, body)
-        else:
-            form.username.errors.append("This user does not exist.")
-    return render_template("add_mate.html", title="Add Mates", form=form, response=response)
-
-@app.route("/mate_requests")
-@login_required
-def mate_requests():
-    user = session["username"]
-    db = get_db()
-    invites = db.execute("""
-                    SELECT *
-                    FROM invites
-                    WHERE receiver = ?""", (user,))
-    pending_requests = db.execute("""
-                    SELECT *
-                    FROM invites
-                    WHERE sender = ?""", (user,))
-    return render_template("mate_requests.html", title="Add Mates", invites=invites, pending_requests=pending_requests)
+#@app.route("/add_mate", methods=["GET", "POST"])
+#@login_required
+#def add_mate():
+#    response = ""
+#    form = AddMateForm()
+#    if form.validate_on_submit():
+#        sender = session["username"]
+#        receiver = form.username.data
+#        if sender == receiver:
+#            form.username.errors.append("You cannot invite yourself.")
+#            return render_template("add_mate.html", title="Add Mates", form=form, response=response)
+#        db = get_db()
+#        existing_user = db.execute("""
+#                                SELECT *
+#                                FROM users
+#                                WHERE username = ?;
+#                                """, (receiver,)).fetchone()
+#        
+#        existing_invite = db.execute("""
+#                                     SELECT *
+#                                     FROM invites
+#                                     WHERE sender = ? AND receiver = ?;
+#                                     """, (sender, receiver,)).fetchone()
+#        if existing_invite is not None:
+#            form.username.errors.append("You have already sent this user an invite.")
+#        elif existing_user is not None:
+#            db.execute("""
+#                       INSERT INTO invites (sender, receiver)
+#                       VALUES
+#                       (?, ?);
+#                       """, (sender, receiver,))
+#            db.commit()
+#            response = "Mate Request Sent"
+#            title = "New Mate Request! 💊"
+#            body = f"{sender} has sent you a MediMate request!"
+#            send_email_notification(receiver, title, body)
+#            send_push_notification(receiver, title, body)
+#        else:
+#            form.username.errors.append("This user does not exist.")
+#    return render_template("add_mate.html", title="Add Mates", form=form, response=response)
     
 @app.route("/cancel_request/<string:receiver>")
 @login_required
@@ -681,7 +664,7 @@ def cancel_request(receiver):
                          FROM invites
                          WHERE sender = ?
                          """, (sender,))
-    return render_template("pending_requests.html", title="Pending Requests", invites=invites)
+    return redirect(url_for("medimates"))
 
 @app.route("/accept_request/<string:friend1>")
 @login_required
@@ -717,7 +700,7 @@ def accept_request(friend1):
                          FROM invites
                          WHERE receiver = ?
                          """, (friend2,))
-    return render_template("mate_requests.html", title="Mate Requests", invites=invites)
+    return redirect(url_for("medimates"))
         
 @app.route("/reject_request/<string:sender>")
 @login_required
@@ -739,18 +722,64 @@ def reject_request(sender):
                          FROM invites
                          WHERE sender = ?
                          """, (sender,))
-    return render_template("mate_requests.html", title="Mate Requests", invites=invites)
+    return redirect(url_for("medimates"))
 
-@app.route("/medimates")
+@app.route("/medimates", methods=["GET", "POST"])
 @login_required
 def medimates():
-    user = session["username"]
+    response = ""
     db = get_db()
+    user = session["username"]
+    form = AddMateForm()
+    if form.validate_on_submit():
+        receiver = form.username.data
+        if user == receiver:
+            form.username.errors.append("You cannot invite yourself.")
+            return render_template("add_mate.html", title="Add Mates", form=form, response=response)
+        existing_user = db.execute("""
+                                SELECT *
+                                FROM users
+                                WHERE username = ?;
+                                """, (receiver,)).fetchone()
+        
+        existing_invite = db.execute("""
+                                     SELECT *
+                                     FROM invites
+                                     WHERE sender = ? AND receiver = ?;
+                                     """, (user, receiver,)).fetchone()
+        if existing_invite is not None:
+            form.username.errors.append("You have already sent this user an invite.")
+        elif existing_user is not None:
+            db.execute("""
+                       INSERT INTO invites (sender, receiver)
+                       VALUES
+                       (?, ?);
+                       """, (user, receiver,))
+            db.commit()
+            response = "Mate Request Sent"
+            title = "New Mate Request! 💊"
+            body = f"{user} has sent you a MediMate request!"
+            send_email_notification(receiver, title, body)
+            send_push_notification(receiver, title, body)
+        else:
+            form.username.errors.append("This user does not exist.")
+
     medimates = db.execute("""
                     SELECT *
                     FROM friends
                     WHERE friend1 = ?""", (user,))
-    return render_template("medimates.html", title="Medimates", medimates=medimates)
+    
+    invites = db.execute("""
+                    SELECT *
+                    FROM invites
+                    WHERE receiver = ?""", (user,))
+    pending_requests = db.execute("""
+                    SELECT *
+                    FROM invites
+                    WHERE sender = ?""", (user,))
+    
+
+    return render_template("medimates.html", title="Medimates", medimates=medimates, invites=invites, pending_requests=pending_requests, form=form, response=response)
 
 @app.route("/remove_medimate/<string:friend>")
 @login_required
@@ -773,7 +802,7 @@ def remove_medimate(friend):
                     SELECT *
                     FROM friends
                     WHERE friend1 = ?""", (user,))
-    return render_template("medimates.html", title="Medimates", medimates=medimates)
+    return redirect(url_for("medimates"))
 
     
 
